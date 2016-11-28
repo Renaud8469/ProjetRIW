af4b83ac15c4266a3cebe0709b056736f7979d1c
paulden <paul.de-nonancourt@student.ecp.fr>
2016-11-28 16:16:02 +0100

    Calculating number of tokens in CACM

--- nltkOnCACM.py
+++ nltkOnCACM.py
@@ -11,9 +11,13 @@ tokensCACM = []
 for line in cacm:
     if ".I " in line:
         idPaper = get_id(line)
+        print(idPaper)
     if (".T" in line) or (".W" in line) or (".B" in line) or (".A" in line) or (".N" in line) or (".X" in line) or (".K" in line):
         latestMark = line
     if (".T" in latestMark) or (".W" in latestMark) or (".K" in latestMark):
-        tokensCACM = tokensCACM + nltk.word_tokenize(line)
+        if len(line) > 3:
+            currentTokens = nltk.word_tokenize(line)
+            tokensCACM = tokensCACM + currentTokens
 
-print(tokensCACM)
+tokensNumber = len(tokensCACM)
+print(tokensNumber)

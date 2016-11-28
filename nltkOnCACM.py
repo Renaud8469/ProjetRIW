142fe72e75d41ae643657f213b69a177dbfb60e3
Renaud Dahl <renaud.dahl@student.ecp.fr>
2016-11-28 16:24:24 +0100

    Add common words comparison function

--- nltkOnCACM.py
+++ nltkOnCACM.py
@@ -1,4 +1,5 @@
 import nltk
+from common_words_comp import *
 
 cacm = open('CACM/cacm.all', 'r')
 
@@ -11,7 +12,6 @@ tokensCACM = []
 for line in cacm:
     if ".I " in line:
         idPaper = get_id(line)
-        print(idPaper)
     if (".T" in line) or (".W" in line) or (".B" in line) or (".A" in line) or (".N" in line) or (".X" in line) or (".K" in line):
         latestMark = line
     if (".T" in latestMark) or (".W" in latestMark) or (".K" in latestMark):
@@ -21,3 +21,6 @@ for line in cacm:
 
 tokensNumber = len(tokensCACM)
 print(tokensNumber)
+
+print(tokensCACM[0:20])
+print(lower_and_remove_common(tokensCACM)[0:20])

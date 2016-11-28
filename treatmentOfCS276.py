ac4d6741f161f7e91709b12040904481ba713634
Renaud Dahl <renaud.dahl@student.ecp.fr>
2016-11-28 17:17:11 +0100

    Add functions

--- treatmentOfCS276.py
+++ treatmentOfCS276.py
@@ -1,15 +1,47 @@
-import nltk,os
+import nltk
+import os
+from common_words_comp import lower_and_remove_common
 
 global_directory = "CS276/pa1-data/"
 
-count = 0
 
-for i in range(0, 9):
-    for file in os.listdir(global_directory + str(i)):
-        count += 1
+def count_tokens_cs76():
+    token_count = 0
+    for i in range(0, 9):
+        for filename in os.listdir(global_directory + str(i)):
+            words = open(global_directory + str(i) + '/' + filename).readline()
+            tokens = nltk.word_tokenize(words)
+            token_count += len(tokens)
 
-print(count)
+    return token_count
 
 
+def count_vocabulary_cs76():
+    vocabulary = []
+    for i in range(0, 9):
+        for filename in os.listdir(global_directory + str(i)):
+            words = open(global_directory + str(i) + '/' + filename).readline()
+            tokens = nltk.word_tokenize(words)
+            for word in tokens:
+                if word not in vocabulary:
+                    vocabulary.append(word)
+    return len(vocabulary)
 
 
+def count_tokens_and_vocabulary():
+    vocabulary = []
+    token_count = 0
+    for i in range(0, 9):
+        for filename in os.listdir(global_directory + str(i)):
+            words = open(global_directory + str(i) + '/' + filename).readline()
+            tokens = nltk.word_tokenize(words)
+            token_count += len(tokens)
+            for word in lower_and_remove_common(tokens):
+                if word not in vocabulary:
+                    vocabulary.append(word)
+
+    print("This collection has " + str(token_count) + " tokens.")
+    print("Its vocabulary is made of " + str(len(vocabulary)) + "words.")
+
+
+count_tokens_and_vocabulary()

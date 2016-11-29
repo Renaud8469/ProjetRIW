ee5e54944dbabf548f5f6c4cdf5a46def4d95c65
Renaud Dahl <renaud.dahl@student.ecp.fr>
2016-11-29 16:20:47 +0100

    Remove numbers from vocabulary and tokens

--- get_token_list.py
+++ get_token_list.py
@@ -1,19 +1,22 @@
 import nltk
+from useful_functions import custom_tokenize
+
 
 def get_id(x):
     return int(''.join(ele for ele in x if ele.isdigit()))
 
+
 def get_token_list(file):
     token_list = []
-    latestMark = ""
+    latest_mark = ""
     for line in file:
         if ".I " in line:
             id_paper = get_id(line)
         if (".I " in line) or (".T" in line) or (".W" in line) or (".B" in line) or (".A" in line) or (".N" in line) or (".X" in line) or (".K" in line):
-            latestMark = line
-        if (".T" in latestMark) or (".W" in latestMark) or (".K" in latestMark):
+            latest_mark = line
+        if (".T" in latest_mark) or (".W" in latest_mark) or (".K" in latest_mark):
             if len(line) > 3:
-                current_tokens = nltk.word_tokenize(line)
+                current_tokens = custom_tokenize(line)
                 token_list = token_list + current_tokens
     return token_list
 

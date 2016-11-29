1e66cb426c5aebd9701db1f1af7bb7ae096e3af0
paulden <paul.de-nonancourt@student.ecp.fr>
2016-11-29 16:00:26 +0100

    Refactoring

--- calculating_parameters.py
+++ calculating_parameters.py
@@ -1,9 +1,9 @@
 from math import *
 
-T=217847
-T_prime=108935
-M=11654
-M_prime=8221
+T = 217847
+T_prime = 108935
+M = 11654
+M_prime = 8221
 
 b = log(M_prime/M)/log(T_prime/T)
 k = M/(T**b)
--- test_cacm.py
+++ test_cacm.py
@@ -5,13 +5,10 @@ from common_words_comp import *
 cacm = open('CACM/cacm.all', 'r')
 
 token_cacm = get_token_list(cacm)
-print("Nombre de tokens : ")
-print(len(token_cacm))
+print("Nombre de tokens : " + str(len(token_cacm)))
 
 token_no_duplicates = lower_and_remove_common(token_cacm)
-print("Sans les duplicats : ")
-print(len(token_no_duplicates))
+print("Sans les duplicats : " + str(len(token_no_duplicates)))
 
 vocabulary = set(token_no_duplicates)
-print("Taille du vocabulaire : ")
-print(len(vocabulary))
+print("Taille du vocabulaire : " + str(len(vocabulary)))

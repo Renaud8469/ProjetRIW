0fad1de88a35b0749b6ad1bf069fb1c73ba840ed
paulden <paul.de-nonancourt@student.ecp.fr>
2016-11-30 09:28:24 +0100

    Implementing function get_frequency_list returning a dictionary with terms as keys and frequency as values

--- frequency_rank.py
+++ frequency_rank.py
@@ -4,14 +4,6 @@ from useful_functions import *
 
 cacm = open('CACM/cacm.all', 'r')
 
-token_cacm = get_token_list(cacm)
-token_no_common = lower_and_remove_common(token_cacm)
-
-frequency_list = {}
-for token in token_no_common:
-    if token in frequency_list:
-        frequency_list['token'] = frequency_list['token'] + 1/189000
-    else:
-        frequency_list = frequency_list.update({'token': 1/189000})
+frequency_list = get_frequency_list(cacm)
 
 print(frequency_list)
--- get_token_list.py
+++ get_token_list.py
@@ -1,5 +1,5 @@
 import nltk
-from useful_functions import custom_tokenize
+from useful_functions import *
 
 
 def get_id(x):
@@ -53,19 +53,15 @@ def get_token_number(file):
     return token_number
 
 def get_frequency_list(file):
-    token_number = 189832
-    print(token_number)
+    token_list_pre = get_token_list(file)
+    token_list = lower_and_remove_common(token_list_pre)
+    size = len(token_list)
     frequency_list = {}
-    for line in file:
-        if (".I " in line) or (".T" in line) or (".W" in line) or (".B" in line) or (".A" in line) or (".N" in line) or (".X" in line) or (".K" in line):
-            latest_mark = line
-        if (".T" in latest_mark) or (".W" in latest_mark) or (".K" in latest_mark):
-            if len(line) > 3:
-                current_tokens = custom_tokenize(line)
-                for token in current_tokens:
-                    if token in frequency_list:
-                        frequency_list['token'] = frequency_list['token'] + 1/token_number
-                    else:
-                        frequency_list = frequency_list.update({'token': 1/token_number})
+    for token in token_list:
+        word = token
+        if word in frequency_list:
+            frequency_list[word] = frequency_list[word] + 1/size
+        else:
+            frequency_list[word] = 1/size
     return frequency_list
 

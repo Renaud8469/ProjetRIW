a0a4caa73faa71791d6277939d8d39b8be713dd4
paulden <paul.de-nonancourt@student.ecp.fr>
2016-12-10 14:48:41 +0100

    Refactored files architecture and commented code

--- test_index_bsearch_cacm.py
+++ test_index_bsearch_cacm.py
@@ -10,7 +10,7 @@ reverse_index = get_reverse_index(vocabulary, cacm)
 print(str(len(docs_in_index(reverse_index))) + " documents ont été indexés")
 print(str(len(reverse_index)) + " termes dans l'index inversé \n")
 
-query = input("Entrez votre requête en utilisant AND, OR, NOT et des parenthèses : ")
+query = input("Entrez votre requête en utilisant les mots-clés, AND, OR, NOT et des parenthèses : ")
 
 print("Recherche en cours... \n")
 

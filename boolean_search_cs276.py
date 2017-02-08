cb0580aa039bbdd25da4a391b2bd30b8428475ea
paulden <paul.de-nonancourt@student.ecp.fr>
2017-02-08 15:08:19 +0100

    Displaying title for CS276 (boolean search)

--- boolean_search_cs276.py
+++ boolean_search_cs276.py
@@ -1,11 +1,12 @@
-from indexation_of_cs276 import *
-from first_questions_on_cs276 import count_tokens_and_vocabulary
 from collection import Collection
+from indexation_of_cs276 import *
+from first_questions_on_cs276 import get_vocabulary_cs276
 import time
 
 
-vocabulary = count_tokens_and_vocabulary()
+vocabulary = get_vocabulary_cs276()[0]
 docs, index = make_dictionary("CS276/pa1-data/")
+id_to_doc = make_doc_id_to_doc("CS276/pa1-data/")[0]
 
 cs276_collection = Collection(list(docs.keys()), vocabulary, index)
 
@@ -24,7 +25,7 @@ while 1:
     print(str(len(results)) + " publications correspondantes ont été trouvées : ")
     k = 1
     for i in results:
-        print("\t" + str(k) + "\tPublication n°" + str(i))
+        print("\t" + str(k) + "\tPublication n°" + str(i) + "\t- " + id_to_doc[i])
         k += 1
 
     print("Recherche effectuée en %s seconde(s) \n" % round(finish_time-search_time, 4))

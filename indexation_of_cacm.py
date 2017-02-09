41311f09c646f8cbb987e962c3ec43717ba5ab7d
Paul de Nonancourt <paul.de-nonancourt@student.ecp.fr>
2017-02-09 19:29:24 +0100

    Indexing authors as well

--- indexation_of_cacm.py
+++ indexation_of_cacm.py
@@ -15,7 +15,7 @@ def get_vocabulary_dict(file):
             id_paper = get_id(line)
         if (".I " in line) or (".T" in line) or (".W" in line) or (".B" in line) or (".A" in line) or (".N" in line) or (".X" in line) or (".K" in line):
             latest_mark = line
-        if (".T" in latest_mark) or (".W" in latest_mark) or (".K" in latest_mark):
+        if (".T" in latest_mark) or (".W" in latest_mark) or (".K" in latest_mark) or (".A" in latest_mark):
             if ".T" in latest_mark and line != latest_mark:
                 if id_paper in id_to_doc.keys():
                     id_to_doc[id_paper] += line[:-1]
@@ -48,7 +48,7 @@ def get_reverse_index(vocabulary, file):
         if (".I " in line) or (".T" in line) or (".W" in line) or (".B" in line) or (".A" in line) or (
             ".N" in line) or (".X" in line) or (".K" in line):
             latest_mark = line
-        if (".T" in latest_mark) or (".W" in latest_mark) or (".K" in latest_mark):
+        if (".T" in latest_mark) or (".W" in latest_mark) or (".K" in latest_mark) or (".A" in latest_mark):
             if len(line) > 3:
                 current_tokens = lower_and_remove_common(custom_tokenize(line))
                 for token in current_tokens:

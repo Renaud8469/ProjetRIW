f2da36007347372024844a868b5ccd99758d57b8
Paul de Nonancourt <paul.de-nonancourt@student.ecp.fr>
2017-02-13 20:26:55 +0100

    Using pickle to store efficiently index of CS276

--- vector_search_cs276.py
+++ vector_search_cs276.py
@@ -2,13 +2,26 @@ from collection import Collection
 from indexation_of_cs276 import *
 from first_questions_on_cs276 import get_vocabulary_cs276
 import time
+import pickle
 
 
-vocabulary = get_vocabulary_cs276()[0]
-docs, index = make_dictionary("CS276/pa1-data/")
+try:
+    docs_cs276 = open('static/docs_cs276.txt', 'r')
+    print("Index détecté dans le dossier static/, chargement des fichiers détectés en cours...")
+    index = pickle.load(open('static/index_cs276.p', 'rb'))
+    vocabulary = pickle.load(open('static/voc_cs276.p', 'rb'))
+    docs = get_docs(docs_cs276)
+    print("Index chargé !\n")
+except FileNotFoundError:
+    print("Construction de l'index en cours...")
+    vocabulary = get_vocabulary_cs276()[0]
+    docs, index = make_dictionary("CS276/pa1-data/")
+    docs = list(docs.keys())
+    print("Index construit !\n")
+
 id_to_doc = make_doc_id_to_doc("CS276/pa1-data/")[0]
 
-cs276_collection = Collection(list(docs.keys()), vocabulary, index)
+cs276_collection = Collection(docs, vocabulary, index)
 
 while 1:
     query = input("Entrez votre requête en utilisant les mots-clés séparés par une virgule : ")
